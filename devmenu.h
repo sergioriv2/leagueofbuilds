2aa70c9af4318305adde32033902bbf0e9c558c5
Sergio <62715874+sergioriv2@users.noreply.github.com>
2020-11-01 21:56:36 -0300

    v0.06

--- devmenu.h
+++ devmenu.h
@@ -9,9 +9,16 @@ public:
 	int menuPrincipal();
 	void menuOpciones();
 	void menuCampeones();
-	
+	void menuItems();
+	void menuConjuntos();
+	//Campeones
 	void agregarCampeones();
 	void mostrarCampeones();
 	void modificarCampeon();
 	void eliminarCampeon();
+	//Items
+	void agregarItems();
+	void bajaItems();
+	void mostrarItems();
+	void editarItems();
 };
